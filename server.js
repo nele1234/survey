e767d71e3dae84e5ef7f04bb6e04e2ead022a3ad
nedzad <nedzad.grbo@stu.ibu.edu.ba>
2018-08-08 12:31:32 +0200

    route, main page,

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
 
-app.get('/', express.static(__dirname + "/static"))
+app.use(express.static(__dirname + "/static"))
 
 app.listen(3000, () => console.log('Example app listening on port 3000!'))
