1c68e755b61f78d73f51dc9958587bed79dc4e77
nedzad <nedzad.grbo@stu.ibu.edu.ba>
2018-08-09 11:46:13 +0200

    .

--- server.js
+++ server.js
@@ -1,6 +1,21 @@
-const express = require('express')
-const app = express()
+var express = require('express')
+var app = express()
+var port = process.env.PORT || 3000
 
-app.use(express.static(__dirname + "/static"))
+var mongojs = require('mongojs')
+var db = mongojs('localhost:27017/survey', ['anketa'])
 
-app.listen(3000, () => console.log('Example app listening on port 3000!'))
+var body_parser = require('body-parser')
+app.use(body_parser.json())
+
+app.use(express.static(__dirname + '/static'))
+
+app.get('/anketa', function (req, res) {
+  db.tour.find(function (err, docs) {
+    res.json(docs)
+  })
+})
+
+app.listen(port, function () {
+  console.log('Node app is running on port', port)
+}}
