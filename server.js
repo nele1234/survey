1d96c40762033205fa851834c6e0bdd82d3d32c1
nedzad <nedzad.grbo@stu.ibu.edu.ba>
2018-08-13 10:29:40 +0200

    toastr i post

--- server.js
+++ server.js
@@ -3,19 +3,32 @@ var app = express()
 var port = process.env.PORT || 3000
 
 var mongojs = require('mongojs')
-var db = mongojs('localhost:27017/survey', ['anketa'])
+var db = mongojs('localhost:27017/survey', ['ankete'])
 
 var body_parser = require('body-parser')
 app.use(body_parser.json())
 
+var urlencodedParser = body_parser.urlencoded({
+  extended: false
+})
+
 app.use(express.static(__dirname + '/static'))
 
 app.get('/anketa', function (req, res) {
-  db.tour.find(function (err, docs) {
+  db.ankete.find(function (err, docs) {
+    res.json(docs)
+  })
+})
+
+app.post('/anketa', urlencodedParser, function (req, res, next) {
+  console.log(req.body)
+
+  db.ankete.insert(req.body, function (err, docs) {
+    console.log('inserted')
     res.json(docs)
   })
 })
 
 app.listen(port, function () {
   console.log('Node app is running on port', port)
-}}
+})
--- static/controllers/getSurvey.js
+++ static/controllers/getSurvey.js
@@ -1,9 +1,33 @@
-function getSurvey($scope, $http) {
-  getting();
- function getting(){
-    $http.get("/ankete")
+function getSurvey($scope, $http, toastr) {
+
+var getting = function(){
+    $http.get("/anketa")
     .then(function(response) {
         $scope.myWelcome = response.data;
     });
 }
+getting();
+
+$scope.addAnketa = function () {
+    console.log('anekta dodana')
+    $http.post('/anketa', $scope.anketa).then(function (response) {
+      console.log(response)
+      /*$scope.tour.tourName = ''
+      $scope.tour.tourCity = ''
+      $scope.tour.tourDescription = ''
+      $scope.tour.tourImage = ''*/
+      //toastr.success("Tour added successfully")
+      toastr.success("anketa dodana");
+      getting()
+    })
+  }
+
+  $scope.open = function () {
+    $scope.visible = false;
+    $scope.visible = $scope.visible = true;
+  }
+  $scope.close = function () {
+    $scope.visible = true;
+    $scope.visible = $scope.visible = false;
+  }
 };
