fda1a7d24d47fdecc4893f470c133fbd4e9e5ba3
nedzad <nedzad.grbo@stu.ibu.edu.ba>
2018-08-15 10:10:34 +0200

    update i delete

--- server.js
+++ server.js
@@ -28,7 +28,53 @@ app.post('/anketa', urlencodedParser, function (req, res, next) {
     res.json(docs)
   })
 })
+app.delete('/deleteSurvey/:id', function (req, res) {
+  var id = req.params.id
+  console.log(id)
+  db.ankete.remove({
+    _id: mongojs.ObjectId(id)
+  }, function (err, doc) {
+    console.log('Survey removed')
+    res.json(doc)
+  })
+})
+app.get('/ankete/:id', urlencodedParser, function (req, res) {
+  var id = req.params.id
+  console.log(id)
+  db.ankete.findOne({
+    _id: mongojs.ObjectId(id)
+  }, function (err, doc) {
+    console.log('selected')
+    res.json(doc)
+  })
+})
+
+app.put('/ankete/:id', function (req, res) {
+  var id = req.params.id
+  console.log(req.body)
+  db.ankete.findAndModify({
+    query: {
+      _id: mongojs.ObjectId(id)
+    },
+    update: {
+      $set: {
+        name: req.body.name,
+        question1: req.body.question1,
+        answer11: req.body.answer11,
+        answer12: req.body.answer12,
+        answer13: req.body.answer13,
+        answer14:req.body.answer14,
+        question2:req.body.question2,
+        answer21:req.body.answer21,
+        answer22:req.body.answer22,
+        answer23:req.body.answer23,
+        answer24:req.body.answer24
+      }
+    }
+   } )
+})
 
 app.listen(port, function () {
   console.log('Node app is running on port', port)
 })
+
--- static/controllers/getSurvey.js
+++ static/controllers/getSurvey.js
@@ -12,11 +12,6 @@ $scope.addAnketa = function () {
     console.log('anekta dodana')
     $http.post('/anketa', $scope.anketa).then(function (response) {
       console.log(response)
-      /*$scope.tour.tourName = ''
-      $scope.tour.tourCity = ''
-      $scope.tour.tourDescription = ''
-      $scope.tour.tourImage = ''*/
-      //toastr.success("Tour added successfully")
       toastr.success("anketa dodana");
       getting()
     })
@@ -29,5 +24,45 @@ $scope.addAnketa = function () {
   $scope.close = function () {
     $scope.visible = true;
     $scope.visible = $scope.visible = false;
+  }
+
+  $scope.deleteSurvey = function (id) {
+    console.log('delete surveS')
+    console.log(id)
+    $http.delete('/deleteSurvey/' + id).then(function (response) {
+      console.log('removed')
+      toastr.error("Survey removed")
+      getting()
+    })
+  }
+
+  $scope.edit = function (id) {
+    console.log('select survey')
+    console.log(id)
+    $http.get('/ankete/' + id).then(function (response) {
+      console.log('selected')
+      $scope.ankete = response.data
+    })
+  }
+
+  $scope.update = function () {
+    console.log('update survey')
+    console.log($scope.ankete._id)
+    $http.put('ankete/' + $scope.ankete._id, $scope.ankete).then(function (response) {
+      console.log('update')
+      $scope.ankete.name = ''
+      $scope.ankete.question1 = ''
+      $scope.ankete.answer11 = ''
+      $scope.ankete.answer12 = ''
+      $scope.ankete.answer13 = ''
+      $scope.ankete.answer14 = ''
+      $scope.ankete.question2 = ''
+      $scope.ankete.answer21=''
+      $scope.ankete.answer22 = ''
+      $scope.ankete.answer23 = ''
+      $scope.ankete.answer24 = ''
+      toastr.success("survey updated successfully")
+      getting()
+    })
   }
 };
