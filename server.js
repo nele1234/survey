60efa9551a6b85da7b45ca868207f758574857c4
nedzad <nedzad.grbo@stu.ibu.edu.ba>
2018-08-26 22:55:46 +0200

    ...

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var app = express();
-var port = process.env.PORT || 3000;
+var port = process.env.PORT || 3001;
 
 const bodyparser = require("body-parser");
 var mongojs = require('mongojs');
